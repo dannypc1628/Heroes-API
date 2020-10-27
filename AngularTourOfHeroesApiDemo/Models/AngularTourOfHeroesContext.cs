78db74b94c77e027e3196db84716b4f49fd93d0d
dannyLiu <h152074716@gmail.com>
2020-10-27 16:49:38 +0800

    dotnet ef dbcontext scaffold

---
+++ AngularTourOfHeroesApiDemo/Models/AngularTourOfHeroesContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AngularTourOfHeroesApiDemo.Models
+{
+    public partial class AngularTourOfHeroesContext : DbContext
+    {
+        public AngularTourOfHeroesContext()
+        {
+        }
+
+        public AngularTourOfHeroesContext(DbContextOptions<AngularTourOfHeroesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Hero> Hero { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source =localhost;Initial Catalog=AngularTourOfHeroes;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Hero>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ AngularTourOfHeroesApiDemo/Models/Hero.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AngularTourOfHeroesApiDemo.Models
+{
+    public partial class Hero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
