d380dca648bd704fb1be208e4810405624e3e5d8
dannyLiu <h152074716@gmail.com>
2020-10-28 16:17:56 +0800

    建立HerosAPI的Get、Post、Put方法

---
+++ AngularTourOfHeroesApiDemo/Controllers/HeroesController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AngularTourOfHeroesApiDemo.Models;
+
+namespace AngularTourOfHeroesApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroesController : ControllerBase
+    {
+        private readonly AngularTourOfHeroesContext _context;
+
+        public HeroesController(AngularTourOfHeroesContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Hero>>> GetHeroes()
+        {
+            var data = await _context.Hero.ToListAsync();
+            return data;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Hero>> GetHeroById(int id)
+        {
+            var hero = await _context.Hero.FindAsync(id);
+
+            if (hero == null)
+            {
+                return NotFound();
+            }
+
+            return hero;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Hero>> AddHero(Hero newHero)
+        {
+            var lastHero = await _context.Hero.OrderByDescending(d => d.Id).FirstOrDefaultAsync();
+            if (lastHero == null)
+            {
+                newHero.Id = 1;
+            }
+            else
+            {
+                newHero.Id = lastHero.Id + 1;
+            }
+
+            await _context.Hero.AddAsync(newHero);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateHero(int id,Hero hero)
+        {
+            if (id != hero.Id)
+            {
+                return BadRequest();
+            }
+            
+            var originalHero = await _context.Hero.FindAsync(hero.Id);
+            if (originalHero == null)
+            {
+                return NotFound();
+            }
+
+            originalHero.Name = hero.Name;
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+    }
+}
